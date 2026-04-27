6379056b9d97f50e8d3b06a8c81ac468944c5a79
anto @ alrescha <trimarsanto@gmail.com>
2026-04-28 08:22:18 +0930

    add roles and groups

--- src/cmsfix2/db/fixtures/seed.py
+++ src/cmsfix2/db/fixtures/seed.py
@@ -4,4 +4,29 @@ GROUPS = [
     # (group_name, [assigned_enumkeys for roles])
     ("Editor", [EDITOR]),
     ("Reviewer", [REVIEWER]),
+    ("SiteManager", [SITE_MANAGE, SITE_MODIFY]),
 ]
+
+ENUMKEYS = [
+    # (enumkey, description)
+    (
+        "@ROLES",
+        None,
+        [
+            (SITE_MANAGE, "Can manage site settings"),
+            (SITE_MODIFY, "Can modify site content"),
+            (EDITOR, "Can edit content"),
+            (REVIEWER, "Can review content"),
+        ],
+    )
+]
+
+SITES = [
+    # (fqdn, groups with access to the site)
+    ("*", "SiteManager"),
+]
+
+NODES = [
+    # (type, site, slug, parent_slug, title, content)
+]
+# EOF
