160ebcdc6dce03c1e72d5e76e38d2990a5502f34
anto @ neuromancer <trimarsanto@gmail.com>
2026-05-02 19:49:43 +0930

    remove unused code

--- src/cmsfix2/lib/app.py
+++ src/cmsfix2/lib/app.py
@@ -6,25 +6,13 @@ from litestar import Litestar
 
 from litestar_pulse.config.app import logger
 from litestar_pulse.lib.app import init_app as lp_init_app
-from litestar_pulse.db import set_initdb_function_factory
-
-
-def cmsfix2_initdb_function_factory() -> Callable[..., Awaitable[dict[str, int]]]:
-
-    from ..db.initdb import initialize_database
-
-    return initialize_database
 
 
 def init_app() -> Litestar:
 
     logger.info("Initializing CMSFix2 application...")
 
-    set_initdb_function_factory(cmsfix2_initdb_function_factory())
-
-    return lp_init_app(
-        lp_prefix="/_mgr", initdb_function_factory=cmsfix2_initdb_function_factory
-    )
+    return lp_init_app(lp_prefix="/_mgr")
 
 
 # EOF
