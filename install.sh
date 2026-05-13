96089379da11417b4dfd1cf112b190ba6e9560a2
anto @ neuromancer <trimarsanto@gmail.com>
2026-05-13 21:11:42 +0930

    update: migrate to pixi-based vvg-box

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@
 # installation script for litestar-pulse [https://github.com/trmznt/litestar-pulse]
 
 # optional variable:
-# - BASEDIR
-# - OMIT
+# - VVG_BASEDIR
+# - VVG_EXCLUDE
+# - CMSFIX2_REPOURL
 
 set -eu
 
@@ -24,23 +25,23 @@ case "$parent" in
 esac
 
 # Parsing arguments
-if [ -t 0 ] && [ -z "${BASEDIR:-}" ]; then
+if [ -t 0 ] && [ -z "${VVG_BASEDIR:-}" ]; then
   printf "Base installation directory? [./cmsfix2] "
-  read BASEDIR
+  read VVG_BASEDIR
 fi
 
 # default value
-BASEDIR="${BASEDIR:-./cmsfix2}"
+VVG_BASEDIR="${VVG_BASEDIR:-./cmsfix2}"
 
-uMAMBA_ENVNAME="${uMAMBA_ENVNAME:-cmsfix2}"
+PIXI_ENVNAME="${PIXI_ENVNAME:-cmsfix2}"
 
-mkdir -p ${BASEDIR}/instances/
+mkdir -p ${VVG_BASEDIR}/instances/
 
 # install litestar-pulse
 source <(curl -L https://raw.githubusercontent.com/trmznt/litestar-pulse/main/install.sh)
 
 echo "Cloning cmsfix2"
-git clone --depth 1 https://github.com/trmznt/cmsfix2.git ${ENVS_DIR}/cmsfix2
+git clone --depth 1 ${CMSFIX2_REPOURL:-https://github.com/trmznt/cmsfix2.git} ${ENVS_DIR}/cmsfix2
 
 # perform 2nd stage installation for CMSFix2
 source ${ENVS_DIR}/cmsfix2/etc/inst-scripts/inst-stage-2.sh
@@ -53,12 +54,12 @@ echo "CMSFix2 has been successfully installed. "
 echo "Please read the docs for further setup."
 echo "The base installation directory (VVG_BASEDIR) is:"
 echo
-echo `realpath ${BASEDIR}`
+echo "$(realpath "${VVG_BASEDIR}")"
 echo
 echo "To activate the basic cmsfix2 environment (eg. for installing"
 echo "or setting up base enviroment directory), execute the command:"
 echo
-echo `realpath ${BASEDIR}`/bin/activate
+echo "    $(realpath "${BINDIR}/activate")"
 echo
 
 # EOF
